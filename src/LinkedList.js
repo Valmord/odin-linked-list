bd3f598a83042ae15640bf6dfb576159bee8b131
TC <nothing@nothing.com>
2024-01-26 17:43:37 +1300

    Added at(index), pop(), contains(value) find(value) toString methods

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -10,6 +10,14 @@ export default class LinkedList {
     return node;
   }
 
+  get head() {
+    return this.head;
+  }
+
+  get tail() {
+    return this.tail;
+  }
+
   set tail(node) {
     this.tail = node;
     return node;
@@ -28,6 +36,56 @@ export default class LinkedList {
     if (this.size === 0) [this.head, this.tail] = [node, node];
     else [node.next, this.head] = [this.head, node];
     this.size += 1;
+  }
+
+  at(index) {
+    if (index > this.size) return "Index cannot exceed list size";
+    let node = this.head;
+    for (let i = 0; i <= index; i++) {
+      if (index === i) return node;
+      node = node.next;
+    }
+  }
+
+  find(value) {
+    let node = this.head;
+    let count = 0;
+    while (node !== null) {
+      if (node.data === value) return count;
+      node = node.next;
+      count += 1;
+    }
+    return null;
+  }
+
+  pop() {
+    let node = this.head;
+    while (node.next !== this.tail) {
+      node = node.next;
+    }
+    const tmp = this.tail;
+    [this.tail, node.next] = [node, null];
+
+    return tmp;
+  }
+
+  contains(value) {
+    let node = this.head;
+    while (node !== null) {
+      if (node.data === value) return true;
+      node = node.next;
+    }
+    return false;
+  }
+
+  toString(node = this.head) {
+    let nodeString = "";
+    while (node !== null) {
+      nodeString += `(${node.data}) → `;
+      node = node.next;
+    }
+    nodeString += "(null)";
+    return nodeString;
   }
 
   print(node = this.head) {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ const list = new LinkedList();
 list.append(5);
 list.append(7);
 list.prepend(1);
+list.prepend(9);
 console.dir(list);
-
+console.log(list.size);
+console.log("head:", list.head);
+console.log("tail:", list.tail);
+console.log("at:", list.at(2));
+console.log("pop:", list.pop());
+console.log("contains 5:", list.contains(5));
+console.log("contains -1:", list.contains(-1));
+console.log("find index of 5:", list.find(5));
+console.log("find index of -1:", list.find(-1));
+console.log(list.toString());
 list.print();
