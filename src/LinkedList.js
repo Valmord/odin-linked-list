482e9d1bb7980826410fbadd20a74666439499eb
TC <nothing@nothing.com>
2024-01-26 17:20:05 +1300

    Added LinkedList class and append/prepend methods

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,5 +1,41 @@
 import Node from "./Node";
 
-export default class LinkedList {}
+export default class LinkedList {
+  head = null;
+  tail = null;
+  size = 0;
 
-const list = new LinkedList();
+  set head(node) {
+    this.head = node;
+    return node;
+  }
+
+  set tail(node) {
+    this.tail = node;
+    return node;
+  }
+
+  append(value) {
+    const node = new Node(value);
+    if (this.head === null) this.head = node;
+    if (this.tail !== null) this.tail.next = node;
+    this.tail = node;
+    this.size += 1;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.size === 0) [this.head, this.tail] = [node, node];
+    else [node.next, this.head] = [this.head, node];
+    this.size += 1;
+  }
+
+  print(node = this.head) {
+    const nodeArray = [];
+    while (node !== null) {
+      nodeArray.push(node.data);
+      node = node.next;
+    }
+    console.log(nodeArray);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import LinkedList from "./LinkedList";
 
 const list = new LinkedList();
+list.append(5);
+list.append(7);
+list.prepend(1);
+console.dir(list);
+
+list.print();
